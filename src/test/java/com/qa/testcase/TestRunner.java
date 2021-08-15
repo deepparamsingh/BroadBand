d272d2326a8992e6b278184e98096f5055cced15
paramdeep singh <paramdeep.singh@debutinfotech.com>
2021-08-15 22:55:41 +0530

    15-08-21  10:55pm

---
+++ src/test/java/com/qa/testcase/TestRunner.java
@@ -0,0 +1,20 @@
+//package com.qa.testcase;
+//
+//import org.testng.TestNG;
+//
+//import com.qa.pages.OtpPage;
+//
+//public class TestRunner {
+//
+//	static TestNG testNg;
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		ExtentReportListner ext= new ExtentReportListner();
+//	testNg = new TestNG();
+//	testNg.setTestClasses(new class[] {OtpPage.class});
+//	testNg.addListener(ext);
+//	testNg.run();
+//
+//	}
+//
+//}
